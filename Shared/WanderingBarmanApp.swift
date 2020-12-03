9891619668dc1cf6503091ea9386254e76320b43
Nicholas Galasso <nick.galasso@sevenrooms.com>
2020-12-03 09:43:23 -0500

    Initial Commit

---
+++ Shared/WanderingBarmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderingBarmanApp.swift
+//  Shared
+//
+//  Created by Nick on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct WanderingBarmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
