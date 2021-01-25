f3b8e3020eaafa41406bc5d44b79e1ff46e0d911
Nicholas Galasso <nick.galasso@sevenrooms.com>
2021-01-25 10:32:00 -0700

    confifg

---
+++ Shared/Models/Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  MenuWriter
+//
+//  Created by Nicholas Galasso on 1/25/21.
+//
+
+import Foundation
+
+struct Configuration: Codable {
+    var menu:Menu
+    var hours:String
+    var deliveryDistanceMeters:Int
+}
